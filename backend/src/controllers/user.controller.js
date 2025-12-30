1fd9cc9e303e40d162e5f4f73fa817dddb0af0af
David Gilkey <dav1138@yahoo.com>
2025-12-30 15:44:04 -0500

    second commit

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,41 @@
+import { User } from "../models/user.model.js";
+
+const registerUser = async (requestAnimationFrame, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        // basic validation
+
+        if (!username || !email || !password) {
+            return res.status(400).json({ message: "All fields are important!"})
+        }
+
+        // check user already exists already
+
+        const existing = await User.findOne({ email: email.toLowerCase() });
+        if (existing) {
+            return res.status(400).json({ message: "User already exists!" });
+        }
+
+        // create user
+
+        const user = await User.create({
+            username,
+            email: email.toLowerCase(),
+            password,
+            loggedIn: false
+        });
+
+        res.status(201).json({
+            message: "User registered",
+            user: { id: user._id, email: user.email, username: user.username }
+        })
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+};
+
+export {
+    registerUser
+}
